ec04643a3ea7c3944debc0f6994530f6a33cd5c6
HumJ <tlos142857@live.cn>
2023-06-04 00:14:39 +0800

    DMA优化与示例

---
+++ HumJ.Iot.GU256X128C/GU_DMA.cs
@@ -0,0 +1,80 @@
+﻿namespace HumJ.Iot.GU256X128C
+{
+    public static class GU_DMA
+    {
+        /// <summary>
+        /// Write bit image data to the specified address.
+        /// </summary>
+        /// <param name="dad">Display address</param>
+        /// <param name="a">Bit image write address</param>
+        /// <param name="dn">Bit image data</param>
+        public static byte[] BitImageWrite(byte dad, int a, byte[] dn)
+        {
+            var s = dn.Length;
+
+            var aL = (byte)(a >> 0);
+            var aH = (byte)(a >> 8);
+            var sL = (byte)(s >> 0);
+            var sH = (byte)(s >> 8);
+
+            var bytes = new byte[] { 0x02, 0x44, dad, 0x46, aL, aH, sL, sH }.Concat(dn).ToArray();
+            return bytes;
+        }
+
+        /// <summary>
+        /// Write bit image data to the specified area.
+        /// </summary>
+        /// <param name="dad">Display address</param>
+        /// <param name="a">Bit image write start address</param>
+        /// <param name="sX">Bit image write size X</param>
+        /// <param name="sY">Bit image write size Y</param>
+        /// <param name="dn">Bit image data</param>
+        public static byte[] BoxAreaBitImageWrite(byte dad, int a, int sX, int sY, byte[] dn)
+        {
+            var aL = (byte)(a >> 0);
+            var aH = (byte)(a >> 8);
+            var sXL = (byte)(sX >> 0);
+            var sXH = (byte)(sX >> 8);
+            var sYL = (byte)(sY >> 0);
+            var sYH = (byte)(sY >> 8);
+
+            var bytes = new byte[] { 0x02, 0x44, dad, 0x42, aL, aH, sXL, sXH, sYL, sYH }.Concat(dn).ToArray();
+            return bytes;
+        }
+
+        /// <summary>
+        /// Set the Display start address.
+        /// </summary>
+        /// <param name="dad">Display address</param>
+        /// <param name="a">Display start address</param>
+        public static byte[] DisplayStartAddress(byte dad, int a)
+        {
+            var aL = (byte)(a >> 0);
+            var aH = (byte)(a >> 8);
+
+            var bytes = new byte[] { 0x02, 0x44, dad, 0x53, aL, aH };
+            return bytes;
+        }
+
+        /// <summary>
+        /// Synchronizes the next command with internal display refresh cycle.
+        /// </summary>
+        /// <param name="dad">Display address</param>
+        public static byte[] DisplaySynchronous(byte dad)
+        {
+            var bytes = new byte[] { 0x02, 0x44, dad, 0x57, 0x01 };
+            return bytes;
+        }
+
+        /// <summary>
+        /// Set brightness level.
+        /// </summary>
+        /// <param name="dad">Display address</param>
+        /// <param name="n">Brightness level setting</param>
+        public static byte[] BrightnessLevel(byte dad, BrightnessLevel n)
+        {
+            var bytes = new byte[] { 0x02, 0x44, dad, 0x58, (byte)n };
+            return bytes;
+        }
+    }
+}
---
+++ TestDMA/Program.cs
@@ -0,0 +1,50 @@
+﻿using HumJ.Iot.GU256X128C;
+using System.Device.Gpio;
+using System.Linq;
+
+int[] d = new int[] { 27, 18, 17, 15, 14, 4, 3, 2 };
+int wr = 22;
+int rdy = 23;
+
+GpioController gpio = new GpioController();
+Initialize();
+
+Console.WriteLine("ready");
+
+var random = new Random();
+var buffer = new byte[256 * 128 / 8];
+while (true)
+{
+    var startTime = DateTime.Now;
+    Anime();
+    var endTime = DateTime.Now;
+
+    var delay = endTime - startTime;
+    Console.Title = $"{1000 / delay.TotalMilliseconds} FPS";
+}
+
+void Initialize()
+{
+    gpio.OpenPin(rdy, PinMode.Input);
+    foreach (var p in d.Append(wr))
+    {
+        gpio.OpenPin(p, PinMode.Output, 0);
+    }
+}
+
+void Anime()
+{
+    random.NextBytes(buffer);
+    var bytes = GU_DMA.BitImageWrite(0, 0, buffer);
+    foreach (var b in bytes)
+    {
+        SendByte(b);
+    }
+}
+
+void SendByte(byte b)
+{
+    while (gpio.Read(rdy) == 0) ;
+    gpio.Write(d.Select((p, i) => new PinValuePair(p, (b >> i) & 1)).Append(new PinValuePair(wr, 0)).ToArray());
+    gpio.Write(wr, 1);
+}
